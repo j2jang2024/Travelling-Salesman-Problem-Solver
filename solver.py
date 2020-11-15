a5694e821f0307decacea999c915456149f8f9f8
Macintosh <macintosh@MacBook-Pro.local>
2020-11-15 14:21:46 +0900

    add code 1

---
+++ solver.py
@@ -0,0 +1,74 @@
+from math import sqrt
+import random
+
+def readTSP(filename):
+    f = open(filename,"r")
+    myList = []
+    for line in f:
+        lineSplitted = line.split()
+        if len(lineSplitted) != 3:
+            continue
+        myList.append((float(lineSplitted[1]), float(lineSplitted[2])))
+    return myList
+
+def pythagoras(p1, p2):
+    d = sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+    return d
+
+def getTotalDistance(order):
+    dis = 0
+    for i in range(len(order)-1):
+        dis += pythagoras(order[i], order[i+1])
+    dis += pythagoras(order[-1], order[0])
+    return dis
+
+def greedySeed(points):
+    templen = len(points)
+    distance = 0
+    order = []
+    first_point = points.pop(random.choice(range(len(points))))
+    
+    for x in range(templen-1):
+        current = points[0]
+        for point in points:
+            if pythagoras(first_point, point) < pythagoras(first_point, current):
+                current = point
+        order.append(current)
+        distance += pythagoras(first_point, current)
+        first_point = current
+        points.remove(current)
+    return order
+    
+def randomSeed(points):
+    random.shuffle(points)
+    return points
+
+def twoPointSwap(order):
+    old_dist = getTotalDistance(order)
+    a = random.choice(range(len(order)))
+    b = random.choice(range(len(order)))
+    while a == b:
+        b = random.choice(range(len(order)))
+
+    a_temp = order[a]
+    order[a] = order[b]
+    order[b] = a_temp
+
+    new_dist = getTotalDistance(order)
+    if new_dist < old_dist:
+        return order, new_dist
+    return False, new_dist
+
+init = readTSP('burma14.tsp')
+init = randomSeed(init)
+print("Initial Distance: " + str(getTotalDistance(init)))
+for i in range(30):
+    a, b = twoPointSwap(init)
+    if a == False:
+        print("Failure " + str(b)) 
+    else:
+        print("Success! " + str(b))
+        init = a
+
+#print(greedySeed(readTSP("burma14.tsp")))
+#print(randomSeed(readTSP("burma14.tsp")))
